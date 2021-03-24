1faf4e5958161cb7f6141707f91a0a8090a15300
kartik <kavimandankartik33@gmail.com>
2021-03-24 16:22:43 +0700

    Added: Player Start Point

--- snakeladder.java
+++ snakeladder.java
@@ -1,1 +1,8 @@
-#Welcome the Snake Ladder Game Problem.
+public class snakeladder
+{
+	public static void main(String grgs[])
+	{
+		int startpoint = 0;
+		System.out.println("PlayerStartPoint:" +startpoint);
+	}
+}
