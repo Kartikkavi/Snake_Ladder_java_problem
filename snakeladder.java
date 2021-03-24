ca616df748c7541addcfe986aff350aa43a01039
kartik <kavimandankartik33@gmail.com>
2021-03-24 16:47:58 +0700

    Added:get num 1 to 6

--- snakeladder.java
+++ snakeladder.java
@@ -1,8 +1,15 @@
+import java.util.Random;
+
 public class snakeladder
 {
 	public static void main(String grgs[])
 	{
 		int startpoint = 0;
 		System.out.println("PlayerStartPoint:" +startpoint);
+		Random random = new Random();
+		int diceroll = (random.nextInt(6))+1;
+
+		System.out.println("Dice Number:"+diceroll);
+
 	}
 }
