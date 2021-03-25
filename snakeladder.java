f18d0090240282bfc49f30e4b2a821d30091e1fd
kartik <kavimandankartik33@gmail.com>
2021-03-26 01:50:21 +0700

    Added: 2 player play the game

--- snakeladder.java
+++ snakeladder.java
@@ -4,52 +4,107 @@ public class snakeladder
 {
         public static void main(String args[])
         {
-                int playerposition = 0,  playerpreviousposition = 0;
+                int player1position = 0,  player1previousposition = 0;
+		int player2position = 0,  player2previousposition = 0;
+                int player1startpoint = 0;
+		int player2startpoint = 0;
+		int Dice1counter = 0;
+		int Dice2counter = 0;
 
-                int startpoint = 0;
-		int counter = 0;
-                while (playerposition <= 100){
+                while ((player1position <= 100) && (player2position <= 100)){
 
 
                 Random random = new Random();
-                int diceroll = (random.nextInt(6))+1;
-		counter++;
+                int dice1roll = (random.nextInt(6))+1;
+		Dice1counter++;
 
                 Random rand = new Random();
-                int Check= rand.nextInt(3);
+                int Check1= rand.nextInt(3);
 
-                 playerpreviousposition = playerposition;
-                switch (Check)
+		
+                Random ran = new Random();
+                int dice2roll = (ran.nextInt(6))+1;
+                Dice2counter++;
+
+                Random ra = new Random();
+                int Check2= (ra.nextInt(3));
+
+
+                 player1previousposition = player1position;
+		 player2previousposition = player2position;
+
+                switch (Check1)
                         {
 
                 case 0 :
-                playerposition = playerposition;
+                player1position = player1position;
                // System.out.println("player same position");
 		break;
 
                 case 1 :
-                playerposition  = diceroll + playerposition;
-                //System.out.println("player move ahead"+diceroll+" position.");
+                player1position  = dice1roll + player1position;
+                //System.out.println("player move ahead"+dice1roll+" position.");
                 break;
 
                 case 2 :
-                playerposition = playerposition - diceroll;
+                player1position = player1position - dice1roll;
                 //System.out.println(" player move back" );
                 break;
 
                 default:
                  System.out.println("Invalid option");
 
-                                }
-                if (playerposition < 0){
-                  playerposition =  startpoint;}
-                else if (playerposition > 100 ){
-                  playerposition =  playerpreviousposition;}
-                else if ( playerposition == 100 ){
+                          }
+
+
+		switch (Check2)
+                        {
+
+                case 0 :
+                player2position = player2position;
+               // System.out.println("player same position");
+                break;
+
+                case 1 :
+                player2position  = dice2roll + player2position;
+                //System.out.println("player move ahead"+dice1roll+" position.");
+                break;
+
+                case 2 :
+                player2position = player2position - dice2roll;
+                //System.out.println(" player move back" );
+                break;
+
+                default:
+                 System.out.println("Invalid option");
+	                }
+
+
+                if (player1position < 0){
+                  player1position =   player1startpoint ;}
+                else if (player1position > 100 ){
+                  player1position =  player1previousposition;}
+                else if ( player1position == 100 ){
                 break ;}
-                  }
-                System.out.println(playerposition);
-                System.out.println("Player win");
-		System.out.println("NUm of time Dice roll:"+counter);
+
+		
+                if (player2position < 0){
+                  player2position =   player2startpoint ;}
+                else if (player2position > 100 ){
+                  player2position =  player2previousposition;}
+                else if ( player2position == 100 ){
+                break ;}
+
+		}
+
+		if ( player1position == 100 ){
+		System.out.println("Player 1 wins.");}
+		else if ( player2position == 100 ){
+		System.out.println("Player 2 wins.");}
+		System.out.println("Num of Time Dice 1 roll=" + Dice1counter);
+	        System.out.println("Num of Time Dice 1 roll=" + Dice2counter);
+
         }
 }
+
+
