7d3af246c6c9309eb6a6aee4d6212a1be0d15718
kartik <kavimandankartik33@gmail.com>
2021-03-25 23:48:56 +0700

    Added: Start Game

--- snakeladder.java
+++ snakeladder.java
@@ -1,6 +1,6 @@
 public class snakeladder
 {
-	public static void main(String grgs[])
+	public static void main(String args[])
 	{
 		int startpoint = 0;
 		System.out.println("PlayerStartPoint:" +startpoint);
