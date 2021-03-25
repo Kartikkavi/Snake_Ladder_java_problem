1508885f9bd8c5838a201783bfc9baf879d2dd3f
kartik <kavimandankartik33@gmail.com>
2021-03-25 22:50:32 +0700

    Added: Final Player position

--- snakeladder.java
+++ snakeladder.java
@@ -2,78 +2,52 @@ import java.util.Random;
 
 public class snakeladder
 {
-        public static void main(String grgs[])
+        public static void main(String args[])
         {
-                int playerposition =0;
+                int playerposition = 0,  playerpreviousposition = 0;
 
                 int startpoint = 0;
                 while (playerposition <= 100){
 
-<<<<<<< HEAD
 
                 Random random = new Random();
                 int diceroll = (random.nextInt(6))+1;
 
 
                 Random rand = new Random();
-                int empCheck= rand.nextInt(3);
+                int Check= rand.nextInt(3);
 
-=======
-	       
-                Random rand = new Random();
-                int empCheck= rand.nextInt(3);
-		int playerposition =0;
-
-                switch (empCheck)
+                 playerpreviousposition = playerposition;
+                switch (Check)
                         {
+		
                 case 0 :
                 playerposition = playerposition;
-                System.out.println("player same position");
+               // System.out.println("player same position");
+		break;
 
                 case 1 :
                 playerposition  = diceroll + playerposition;
-                System.out.println("player move ahead:"+diceroll+" position.");
+                //System.out.println("player move ahead"+diceroll+" position.");
                 break;
 
                 case 2 :
                 playerposition = playerposition - diceroll;
-                System.out.println(" player move back" );
-                break;
-
-                default:
-                 System.out.println("Invalid option");
-        
-		}
->>>>>>> UC3-Player-position
-
-                switch (empCheck)
-                        {
-                case 0 :
-                playerposition = playerposition;
-                System.out.println("player same position");
-
-                case 1 :
-                playerposition  = diceroll + playerposition;
-                System.out.println("player move ahead"+diceroll+" position.");
-                break;
-
-                case 2 :
-                playerposition = playerposition - diceroll;
-                System.out.println(" player move back" );
+                //System.out.println(" player move back" );
                 break;
 
                 default:
                  System.out.println("Invalid option");
 
                                 }
+                if (playerposition < 0){
+                  playerposition =  startpoint;}
+                else if (playerposition > 100 ){
+                  playerposition =  playerpreviousposition;}
+                else if ( playerposition == 100 ){
+                break ;}
                   }
+                System.out.println(playerposition);
                 System.out.println("Player win");
         }
 }
-
-
-
-
-
-
-
